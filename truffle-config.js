9e4f615e4464c2c951d12b983218b1b4d696812c
Сергей Черниенко <cherniyenko@iterya.com>
2017-12-13 19:01:52 +0200

    Syncing install on Windows

--- truffle-config.js
+++ truffle-config.js
@@ -2,30 +2,12 @@ require('dotenv').config();
 require('babel-register');
 require('babel-polyfill');
 
-const HDWalletProvider = require('truffle-hdwallet-provider');
-
-const providerWithMnemonic = (mnemonic, rpcEndpoint) =>
-  new HDWalletProvider(mnemonic, rpcEndpoint);
-
-const infuraProvider = network => providerWithMnemonic(
-  process.env.MNEMONIC || '',
-  `https://${network}.infura.io/${process.env.INFURA_API_KEY}`
-);
-
-const ropstenProvider = process.env.SOLIDITY_COVERAGE
-  ? undefined
-  : infuraProvider('ropsten');
-
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*', // eslint-disable-line camelcase
-    },
-    ropsten: {
-      provider: ropstenProvider,
-      network_id: 3, // eslint-disable-line camelcase
     },
     coverage: {
       host: 'localhost',
@@ -34,15 +16,25 @@ module.exports = {
       gas: 0xfffffffffff,
       gasPrice: 0x01,
     },
-    testrpc: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // eslint-disable-line camelcase
-    },
     ganache: {
       host: 'localhost',
       port: 7545,
       network_id: '*', // eslint-disable-line camelcase
     },
   },
+  mocha: {
+    useColors: true,
+  },
+  solc: {
+    optimizer: {
+      enabled: false,
+      runs: 500,
+    },
+    outputSelection: {
+      // Enable the abi and bytecode outputs of every single contract.
+      '*': {
+        '*': [ 'abi', 'evm.bytecode' ],
+      },
+    },
+  }
 };
